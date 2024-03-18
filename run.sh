2c1dfc1cdcfc99923a2356b74effe4c735260525
“Ricz2024” <batistisrico4@gmail.com>
2024-03-18 23:07:10 +0800

    aw

--- run.sh
+++ run.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
-# PLEASE change all "./zh_en_data" to the path where your data is stored
-
 if [ "$1" = "train" ]; then
-	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en --dev-src=./zh_en_data/dev.zh --dev-tgt=./zh_en_data/dev.en --vocab=./zh_en_data/vocab_zh_en.json --cuda --lr=5e-4 --patience=1 --valid-niter=1000 --batch-size=32 --dropout=.25
+    "Python/Python310/python.exe" run.py train --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en --dev-src=./zh_en_data/dev.zh --dev-tgt=./zh_en_data/dev.en --vocab=./zh_en_data/vocab_zh_en.json --cuda --lr=5e-4 --patience=1 --valid-niter=1000 --batch-size=32 --dropout=.25
 elif [ "$1" = "test" ]; then
-	if [ "$2" = "" ]; then
-        CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --cuda
-	else
-		CUDA_VISIBLE_DEVICES=0 python run.py decode $2 ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --cuda
-	fi
+    if [ "$2" = "" ]; then
+        "Python/Python310/python.exe" run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --cuda
+    else
+        "Python/Python310/python.exe" run.py decode $2 ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt --cuda
+    fi
 elif [ "$1" = "train_local" ]; then
-	python run.py train --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en --dev-src=./zh_en_data/dev.zh --dev-tgt=./zh_en_data/dev.en --vocab=./zh_en_data/vocab_zh_en.json --lr=5e-4
+    "Python/Python310/python.exe" run.py train --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en --dev-src=./zh_en_data/dev.zh --dev-tgt=./zh_en_data/dev.en --vocab=./zh_en_data/vocab_zh_en.json --lr=5e-4
 elif [ "$1" = "test_local" ]; then
-    python run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt
+    "Python/Python310/python.exe" run.py decode model.bin ./zh_en_data/test.zh ./zh_en_data/test.en outputs/test_outputs.txt
 elif [ "$1" = "vocab" ]; then
-	python vocab.py --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en ./zh_en_data/vocab_zh_en.json		
+    "Python/Python310/python.exe" vocab.py --train-src=./zh_en_data/train.zh --train-tgt=./zh_en_data/train.en ./zh_en_data/vocab_zh_en.json
 else
-	echo "Invalid Option Selected"
+    echo "Invalid Option Selected"
 fi
